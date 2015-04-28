5ad95cac532ef9865161c758c2d11a838bb8bdff
Marvin Barretto <marvinbarretto@gmail.com>
2015-04-28 16:31:37 +0100

    Fix instagram issue

--- assets/instagram.js
+++ assets/instagram.js
@@ -6,51 +6,29 @@ $(function(){
       // clientId: '783b41c8a8764ddd8926793e79726e61'
       get: 'user',
       userId: 1571068740,
-      accessToken: '1571068740.467ede5.cb9c1a592e0041cc91994b2e0476c5f6',
+      accessToken: '1571068740.467ede5.729fb93c3ba54df5b9e3dff7e373e3dc',
       template: '<a target="_blank" href="{{link}}" class="polaroid third"><img src="{{image}}"><p>{{caption}}</p></a>',
-      resolution: 'low_resolution',
-      before: beforeFunc, 
-      after: afterFunc
+      resolution: 'low_resolution'
+      // before: beforeFunc, 
+      // after: afterFunc
 
   });
   feed.run();
 
-  function beforeFunc(model){
-    console.log(model || "before");
-  }
+  // function afterFunc(model){
+  //   console.log(model || "after func");
 
-  function afterFunc(model){
-    console.log(model || "after func");
-
-    var images = $("#instafeed").find('a');
-    $.each(images, function(index, image) {
-      var delay = (index * 75) + 'ms';
-      $(image).css('-webkit-animation-delay', delay);
-      $(image).css('-moz-animation-delay', delay);
-      $(image).css('-ms-animation-delay', delay);
-      $(image).css('-o-animation-delay', delay);
-      $(image).css('animation-delay', delay);
-      $(image).addClass('animated flipInX');
-    });
-
-  }
-
-  // $.getJSON('http://www.telize.com/geoip?callback=?', function(json) {
-  //   console.log('The IP address is: ' + json.ip);
-  //   console.log('Latitude: ' + json.latitude);
-  //   console.log('Longitude: ' + json.longitude);
-
-  //   // console.log(json);
-  //   var items = [];
-
-  //   $.each( json, function( key, val) {
-  //     items.push( "<li>" + val + "</li>");
+  //   var images = $("#instafeed").find('a');
+  //   $.each(images, function(index, image) {
+  //     var delay = (index * 75) + 'ms';
+  //     $(image).css('-webkit-animation-delay', delay);
+  //     $(image).css('-moz-animation-delay', delay);
+  //     $(image).css('-ms-animation-delay', delay);
+  //     $(image).css('-o-animation-delay', delay);
+  //     $(image).css('animation-delay', delay);
+  //     $(image).addClass('animated flipInX');
   //   });
 
-  //   $( "<ul />", {
-  //     "class": "list",
-  //     html: items.join("")
-  //   }).appendTo("#insta");
-  // });
+  // }
 
 });
